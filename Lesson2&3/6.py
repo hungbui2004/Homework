5a828dc673c42e9e7c30d4a4d6463dc15fe5d1cd
hungbui2004 <quanghungbui8104@gmail.com>
2021-07-21 17:31:35 +0700

    Clean up

--- Lesson2&3/6.py
+++ Lesson2&3/6.py
@@ -1,8 +1,9 @@
-playlist = {}
+
 def makePlaylist(song, artists):
+    playlist = {}
     for i in range(0,len(song),1):
         playlist[song[i]] = artists[i]
     return playlist
 
-print(makePlaylist(["Tang Thượng 102", "Đốt", "Cho Tôi Đi Theo"], ["Cá Hồi Hoang",
+print(makePlaylist(["Tầng Thượng 102", "Đốt", "Cho Tôi Đi Theo"], ["Cá Hồi Hoang",
 "Ngọt", "Ngọt"]))
