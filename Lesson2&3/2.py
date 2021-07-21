6d4954d3a2361a78191807fd51ec1ae7fc20050f
Hung.Q.Bui <85502878+hungbui2004@users.noreply.github.com>
2021-07-21 23:33:26 +0700

    Update 2.py and 6.py

--- Lesson2&3/2.py
+++ Lesson2&3/2.py
@@ -1,16 +1,17 @@
 def typeOfTriangle(x,y,z):
-    if (x <= 0 or y <= 0 or z <= 0 or x > 10**9 or y > 10**9 or z > 10**9):
-        return "Không thể xác định"
-    arr = [x,y,z]
-    newArr = sorted(arr, reverse=False)
-    if (newArr[0] == newArr[1] == newArr[2]):
-        return "Tam giác đều"
-    elif (newArr[0] == newArr[1] or newArr[1] == newArr[2]):
-        return "Tam giác cân"
-    elif (newArr[0] + newArr[1] > newArr[2]):
-        return "Tam giác thường"
+    if (x and y and z > 0) or (x and y and z > 10**9):
+        arr = [x,y,z]
+        newArr = sorted(arr, reverse=False)
+        if (newArr[0] == newArr[1] == newArr[2]):
+            return "Tam giác đều"
+        elif (newArr[0] == newArr[1] or newArr[1] == newArr[2]):
+            return "Tam giác cân"
+        elif (newArr[0] + newArr[1] > newArr[2]):
+            return "Tam giác thường"
+        else:
+            return "Không tồn tại tam giác"
     else:
-        return "Không tồn tại tam giác"
+        return "Không thể xác định"
 
 print(typeOfTriangle(4,4,4))
 
